e8405471c06ce8b33e505c8a047e173778458b1e
Siva Krishna <shivataraya@gmail.com>
2020-01-09 20:09:29 -0600

    add controllers to delegate routes logic

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,30 +1,23 @@
 const express = require("express");
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp
+} = require("../controllers/bootcamp");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: "Show all bootcamps - Hara Hara" });
-});
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-});
-router.post("/", (req, res) => {
-  res
-    .status(201)
-    .json({ success: true, msg: "Create new bootcamp - Hara Hara" });
-});
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+router
+  .route("/")
+  .get(getBootcamps)
+  .post(createBootcamp);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
