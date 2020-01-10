9edb4144cf5fa716ccbdf32e9f3bc63ec15c390d
Siva Krishna <shivataraya@gmail.com>
2020-01-09 19:51:03 -0600

    add crud routes for bootcamps

--- server.js
+++ server.js
@@ -6,9 +6,35 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+app.get("/api/v1/bootcamps", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: "Show all bootcamps - Hara Hara" });
+});
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+app.post("/api/v1/bootcamps", (req, res) => {
+  res
+    .status(201)
+    .json({ success: true, msg: "Create new bootcamp - Hara Hara" });
+});
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
-  port,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
