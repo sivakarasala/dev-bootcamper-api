3fbf793bd06e8212120a1e7a4a613bfee53df6e9
Siva Krishna <shivataraya@gmail.com>
2020-01-10 20:11:30 -0600

    add colors to logging

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 const connectDB = require("./config/db");
 
 // Load env vars
@@ -26,7 +27,10 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgYellow
+      .underline.bold
+  )
 );
 
 // Handle unhandled promise rejections
