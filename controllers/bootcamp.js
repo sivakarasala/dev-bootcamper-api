f407d25e20b1be88c59e01d142697d1d1264f17c
Siva Krishna <shivataraya@gmail.com>
2020-01-16 19:43:10 -0600

    handle createbootcamp

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -1,14 +1,14 @@
+const Bootcamp = require("../models/Bootcamp");
+
 // @desc Get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = (req, res, next) => {
-  res
-    .status(200)
-    .json({
-      success: true,
-      msg: "Show all bootcamps - Hara Hara",
-      shambho: req.shambho
-    });
+  res.status(200).json({
+    success: true,
+    msg: "Show all bootcamps - Hara Hara",
+    shambho: req.shambho
+  });
 };
 
 // @desc Get single bootcamp
@@ -23,10 +23,17 @@ exports.getBootcamp = (req, res, next) => {
 // @desc Create new bootcamp
 // @route POST /api/v1/bootcamps
 // @access Private
-exports.createBootcamp = (req, res, next) => {
-  res
-    .status(201)
-    .json({ success: true, msg: "Create new bootcamp - Hara Hara" });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc Update bootcamp
